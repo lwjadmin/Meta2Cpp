3de7f3f9ed29aca958382bdbaf32f03710cb77a3
DESKTOP-0K0UJ4Q\ETVUSR <dndwpsla123@naver.com>
2022-09-05 16:43:53 +0900

    20220905 1643 이웅재 : 집작업내용 추가

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+void WorkHome() {
+	std::cout << "Work In Home" << std::endl;
+}
+
 int main()
 {
 	std::cout << "Hello world!" << std::endl;
 
 	std::cout << "Nice to Meet you!" << std::endl;
+
+	WorkHome();
+
 	return 0;
 }
