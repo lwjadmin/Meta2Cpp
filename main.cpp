50e722e2c6b7e4c63ed5919866ca74d78f59cca8
DESKTOP-0K0UJ4Q\ETVUSR <dndwpsla123@naver.com>
2022-09-05 16:47:48 +0900

    20220905 1647 이웅재(회사) : 작업본 최신화

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 void WorkOffice() {
 	std::cout << "Work In Office " << std::endl;
+	std::cout << "Work In Office2 " << std::endl;
+	std::cout << "Work In Office3 " << std::endl;
+	std::cout << "Work In Office4 " << std::endl;
+	std::cout << "Work In Office5 " << std::endl;
+	std::cout << "Work In Office6 " << std::endl;
 }
 void WorkHome() {
 	std::cout << "Work In Home" << std::endl;
