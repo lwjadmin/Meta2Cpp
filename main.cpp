9bd11428c62cb21bdfd231e1c092289f4c94f6df
DESKTOP-0K0UJ4Q\ETVUSR <dndwpsla123@naver.com>
2022-09-05 16:43:30 +0900

    20220905 1643 이웅재 : 회사작업내용 추가

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+void WorkOffice() {
+	std::cout << "Work In Office " << std::endl;
+}
+
 int main()
 {
 	std::cout << "Hello world!" << std::endl;
 
 	std::cout << "Nice to Meet you!" << std::endl;
+
+	WorkOffice();
+
 	return 0;
 }
